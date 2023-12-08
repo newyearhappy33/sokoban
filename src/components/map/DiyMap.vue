b913e607a8b0ecc9d7f1a076b6ddb62a0984eec3
无情战斗机 <upup121380@163.com>
2023-12-08 16:27:52 +0800

    引入自定义绘制地图组件

---
+++ src/components/map/DiyMap.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+<template>
+  <div>123</div>
+</template>
+<style scoped></style>
---
+++ src/views/DrawView/DrawView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import DiyMap from "../../components/map/DiyMap.vue";
+</script>
+<template>
+  <div>
+    <DiyMap />
+  </div>
+</template>
+<style scoped></style>
